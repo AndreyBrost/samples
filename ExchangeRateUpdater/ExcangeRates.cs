15cfc29811d0fe63b0a88ef3fbeea44877fab89d
it26 <it26@otlnal.ru>
2017-03-20 13:25:43 +0700

    Implemented certain getting exchange rate functional
    Getting set of calculated rates from currency code

---
+++ ExchangeRateUpdater/ExcangeRates.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ExchangeRateUpdater
+{
+    public class ExchangeRates
+    {
+        [JsonProperty(PropertyName = "base")]
+        public string Base{get; set;}
+        [JsonProperty(PropertyName = "date")]
+        public DateTime Date { get; set; }
+        [JsonProperty(PropertyName = "rates")]
+        public Dictionary<string, decimal> Rates { get; set; }
+    }
+}
--- ExchangeRateUpdater/ExchangeRateProvider.cs
+++ ExchangeRateUpdater/ExchangeRateProvider.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 
 namespace ExchangeRateUpdater
 {
     public class ExchangeRateProvider
     {
+        private const string URL = "http://api.fixer.io/latest?base={0}";
+
         /// <summary>
         /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
         /// by the source, do not return calculated exchange rates. E.g. if the source contains "EUR/USD" but not "USD/EUR",
@@ -13,7 +15,34 @@ namespace ExchangeRateUpdater
         /// </summary>
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            foreach (Currency source in currencies)
+            {
+                ExchangeRates rates =  GetRate(source);
+                foreach (Currency target in currencies)
+                {
+                    if (source.Code != target.Code)
+                    {
+                        if (rates != null && rates.Rates != null && rates.Rates.ContainsKey(target.Code) == true)
+                            yield return new ExchangeRate(source, target, rates.Rates[target.Code]);
+                        else
+                            continue;
+                    }
+                }
+            }
+        }
+
+        private ExchangeRates GetRate(Currency source)
+        {
+            ExchangeRates res = null;
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(string.Format(URL, source.Code)).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    res = response.Content.ReadAsAsync<ExchangeRates>().Result;
+                }
+            }
+            return res;
         }
     }
 }
